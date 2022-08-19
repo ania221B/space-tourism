2fb8da374470672c728a92d013b5f9eed4673553
ania221B <abuwaj@gmail.com>
2022-08-19 10:18:45 +0200

    main page + menu started

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+/* =========
+/* METHOD 1 - WITH CLASSES
+
 const openButton = document.querySelector('.js-open-menu')
 const closeButton = document.querySelector('.js-close-menu')
 console.log(closeButton)
@@ -8,4 +11,23 @@ openButton.addEventListener('click', e => {
 
 closeButton.addEventListener('click', e => {
   document.body.classList.remove('menu-is-open')
+})
+ ========== */
+
+/* ==========
+/* METHOD 2 - WITH DATA ATTRIBUTES */
+
+const nav = document.querySelector('.primary-nav')
+const navToggleButton = document.querySelector('.mobile-nav-toggle')
+
+navToggleButton.addEventListener('click', e => {
+  const visibility = nav.getAttribute('data-visibility')
+
+  if (visibility === 'false') {
+    nav.setAttribute('data-visibility', true)
+    navToggleButton.setAttribute('aria-expanded', true)
+  } else {
+    nav.setAttribute('data-visibility', false)
+    navToggleButton.setAttribute('aria-expanded', false)
+  }
 })
