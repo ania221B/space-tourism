4b45e6b7df90a8f57e9ce3ea0823c7693c1ebde4
ania221B <abuwaj@gmail.com>
2022-08-25 15:51:19 +0200

    destination page part

--- js/script.js
+++ js/script.js
@@ -31,3 +31,56 @@ navToggleButton.addEventListener('click', e => {
     navToggleButton.setAttribute('aria-expanded', false)
   }
 })
+
+/* ============= */
+/* TABBY         */
+/* ============ */
+
+const tabList = document.querySelector('.tabs')
+const tabs = [...tabList.children]
+const contentList = document.querySelector('.tab__content')
+const contentItems = [...contentList.children]
+
+tabList.addEventListener('click', e => {
+  if (!e.target.closest('button')) return
+
+  const tab = e.target.closest('button')
+  const theme = tab.getAttribute('data-theme')
+  const tabContent = contentList.querySelector(`#${theme}`)
+
+  tabs.forEach(tab => {
+    tab.classList.remove('active')
+  })
+  contentItems.forEach(item => {
+    item.classList.remove('active')
+  })
+  tab.classList.add('active')
+  tabContent.classList.add('active')
+})
+
+/* ============= */
+/* METHOD 2       */
+/* ============ */
+
+/* const tabList = document.querySelector('.tabs')
+const tabs = [...tabList.children]
+const contentList = document.querySelector('.tab__content')
+const contentItems = [...contentList.children]
+
+tabList.addEventListener('click', e => {
+  if (!e.target.closest('button')) return
+
+  const tab = e.target.closest('button')
+  const theme = tab.getAttribute('data-theme')
+  const tabContent = contentList.querySelector(`#${theme}`)
+
+  tabs.forEach(t => {
+    t.setAttribute('aria-selected', false)
+  })
+  contentItems.forEach(item => {
+    item.setAttribute('data-visibility', false)
+  })
+  tab.setAttribute('aria-selected', true)
+  tabContent.setAttribute('data-visibility', true)
+})
+*/
